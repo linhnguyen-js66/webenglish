0bc76f431dce4856ef82a01e27e8179b1f33d34d
linhnguyen-js66 <linhnguyenchi227@gmail.com>
2020-07-07 19:35:45 +0700

    Them mot file hackathon43

---
+++ hackathon43/js/aos.js
@@ -0,0 +1,15 @@
+
+			AOS.init({
+				easing: 'ease-out-back',
+				duration: 1000
+			});
+
+        
+			hljs.initHighlightingOnLoad();
+
+			$('.hero__scroll').on('click', function(e) {
+				$('html, body').animate({
+					scrollTop: $(window).height()
+				}, 1200);
+			});
+		
